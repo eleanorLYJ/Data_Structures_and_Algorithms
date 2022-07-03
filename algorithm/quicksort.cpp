e1dc02cd33cfa7f661b676e7fc317e69b749e811
brooklin123 <64023999+brooklin123@users.noreply.github.com>
2022-07-03 09:37:26 +0800

    add quicksort

---
+++ algorithm/quicksort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//閉區間
+void quicksort(int L, int R, vector<int>&vec){
+    if(L > R)return;
+    int pivot = vec[R];
+    int ptr = L; //the position of the value (smaller then pivot) 
+    for(int i = L; i < R; i++){
+        if(vec[i] <= pivot){
+            swap(vec[ptr++], vec[i]);
+        }
+    }
+    swap(vec[ptr], vec[R]);
+    quicksort(L, ptr-1, vec);
+    quicksort(ptr+1, R, vec);
+}
+
+int main(){
+    vector<int>vec = {20, 22, 7, 3};  
+    quicksort(0, vec.size()-1, vec);
+    for(int i = 0; i < vec.size(); i++){
+        cout << vec[i] << " ";
+    }
+    return 0;
+}
