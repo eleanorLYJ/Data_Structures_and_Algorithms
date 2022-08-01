9bc164a4af8f88392722549e689b6fc2adb822b4
brooklin123 <64023999+brooklin123@users.noreply.github.com>
2022-08-01 19:55:21 +0800

    add prime_table.cpp

---
+++ algorithm/prime_table.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#define n 20000
+using namespace std;
+bool table[n];
+
+int main(){
+    for(int i = 2; i < n; i++){
+        table[i] = true;
+    }
+    table[0] = false;
+    table[1] = false;
+    for(int i = 2; i < n; i++){
+        if(table[i]){
+            for(int j = i*i; j < n; j++){
+                if(j%i == 0){
+                    table[j] = false;
+                }
+            }
+        }
+    }
+    cout << table[7] <<endl; 
+    cout << table[30] << endl;
+
+        
+    return 0;
+}
