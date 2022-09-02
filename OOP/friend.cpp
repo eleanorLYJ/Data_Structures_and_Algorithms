0459efe2530576cc38ac13208471903d96bdc220
brooklin123 <64023999+brooklin123@users.noreply.github.com>
2022-09-02 13:51:24 +0800

    fix: add annottation

---
+++ OOP/friend.cpp
@@ -0,0 +1,32 @@
+// practice friend class
+// teacher can know student's score
+#include <iostream>
+using namespace std;
+
+class student{
+    public:
+        student(int s, int score){
+            this->sid = s;
+            this->score = score;
+        }
+        int getter_score(){
+            return score;
+        }
+        friend class teacher;
+    private:
+        int score;
+        int sid;
+};
+class teacher{
+    public:
+        void know_score(student s){
+            cout << s.score;
+        }
+};
+
+int main(){
+    student a(1, 100);
+    teacher tea;
+    tea.know(a);
+    return 0;
+}
