e9d654cd2ed9ae76e6e8779ccce7b2f15c18bd23
Eleanor <count2841@gmail.com>
2023-03-22 20:21:13 +0800

    add

--- algorithm/recursive/GCD.cpp
+++ algorithm/recursive/GCD.cpp
@@ -1,14 +1,15 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
 
 // a is bigger than b
-int GCD(int a, int b){ 
-    if(b == 0) return a;
-    return GCD(b, a%b);
+int GCD(int a, int b) {
+    if (b == 0)
+        return a;
+    return GCD(b, a % b);
 }
 
-int main(){
-    cout << GCD(22,6) << endl;
-    cout << GCD(6,29) << endl;
+int main() {
+    cout << GCD(22, 6) << endl; // 2
+    cout << GCD(6, 29) << endl; // 1
     return 0;
 }
