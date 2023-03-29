3fa93ebcaf641b02b4986d67ef62eef7e1a3e89f
Eleanor <count2841@gmail.com>
2023-03-29 10:04:33 +0800

    rename the file

--- algorithm/sorting/quickselect.c
+++ algorithm/sorting/quickselect.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-// 用quicksort的技巧
-// pivot每次交換的位置 即代表 第lptr位置
+// quickselect同用quicksort的技巧
+// 抽象想法:
+// 比pivot小的往左移動，比pivot大的往右丟，最終pivot與比pivot小中最大的位置+1(或大中最小的-1)交換
+// 結果pivot每次交換的位置 即代表 第lptr位置
+// 平均時間複雜度: O(n)
+// 適用top k考題 (成大資管有出過)
 int main() {
     int n = 5;
     int arr[5] = {10, 5, 6, 4, 2}; // 2 4 5 6 10
@@ -11,7 +15,7 @@ int main() {
         return 0;
     while (1) {
         int lptr = L;
-        int pivot = R;
+        int pivot = R; // pivot選擇最右的陣列元素
         for (int i = L; i < R; i++) {
             if (arr[i] < arr[pivot]) {
                 // swap
@@ -22,11 +26,12 @@ int main() {
                 lptr++;
             }
         }
-        printf("lptr: %d\n", lptr);
         // swap
         int tmp = arr[lptr];
         arr[lptr] = arr[pivot];
         arr[pivot] = tmp;
+
+        // 看每一回合移動了哪些
         printf("%d %d L:%d R: %d \n", arr[pivot], pivot, L, R);
 
         for (int i = 0; i < n; i++) {
