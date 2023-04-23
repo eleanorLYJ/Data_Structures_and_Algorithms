8b3bf372990bdb65ca7b02028185cc181085a6ac
Eleanor <count2841@gmail.com>
2023-04-24 00:31:20 +0800

    add

---
+++ algorithm/graph/shortest_path/Bellman.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <stack>
+#define number 6
+using namespace std;
+
+// 遞迴轉移式: d[i,j] = min(d[i,j], d[i-1,k] + w[k,j])  (k = 1,2,...,n)
+
+int matrix[number][number]; // adjacency matrix
+int dist[number];   // Record the shortest path length from the starting node to
+                    // each node
+int parent[number]; // Record who is the parent of each node on the shortest
+                    // path tree
+bool negative = false; // having negative weight cycle
+void init() {
+    for (int i = 0; i < number; i++) {
+        dist[i] = 1e8;
+    }
+    for (int i = 0; i < number; i++) {
+        parent[i] = i;
+    }
+    for (int i = 0; i < number; i++) {
+        for (int j = 0; j < number; j++) {
+            matrix[i][j] = 1e8;
+        }
+    }
+}
+void bellman_ford(int source) {
+    dist[source] = 0; // Set the shortest path length for the origin
+    // parent[source] = source; // the parent is itself
+
+    for (int i = 0; i < number - 1; i++) // Repeat iterate V-1 times
+        for (int a = 0; a < number; a++) // 全部的邊都當作捷徑
+            for (int b = 0; b < number; b++)
+                if (dist[a] != 1e8 && matrix[a][b] != 1e8 &&
+                    dist[a] + matrix[a][b] < dist[b]) { // a是加進樹裡，b還沒
+                    dist[b] = dist[a] + matrix[a][b];
+                    parent[b] = a;
+                }
+
+    // chack the negative weight cycle
+    // 也就是全部掃過，如果比當前結果更好，便代表有負環
+    for (int i = 0; i < number; i++) {
+        for (int j = 0; j < number; j++) {
+            if (matrix[i][j] != 1e8 && dist[i] != 1e8 &&
+                dist[j] > dist[i] + matrix[i][j]) {
+                cout << "negative weight cycle!" << endl;
+                negative = true;
+                break;
+            }
+        }
+        // 跳出雙層迴圈
+        if (negative)
+            break;
+    }
+    return;
+}
+void printPath(int source, int target) {
+    if (negative)
+        return;
+    stack<int> path;
+    int now = target;
+    bool flag = true; // check whether the valid path
+    while (true) {
+        path.push(now);
+        if (now == source)
+            break;
+        if (now == parent[now]) {
+            flag = false;
+            cout << "cannot reach! \n";
+            break;
+        }
+        now = parent[now];
+    }
+    cout << "path: ";
+    while (flag && !path.empty()) {
+        cout << path.top() << " ";
+        path.pop();
+    }
+}
+int main() {
+    init();
+    matrix[0][1] = matrix[1][0] = -1;
+    matrix[0][2] = matrix[2][0] = -5;
+    matrix[1][2] = matrix[2][1] = -7;
+    matrix[1][3] = matrix[3][1] = 4;
+    matrix[2][4] = matrix[4][2] = 2;
+    matrix[3][5] = matrix[5][3] = 5;
+
+    bellman_ford(0); // source node is 0
+    printPath(0, 5);
+    return 0;
+}
---
+++ algorithm/graph/shortest_path/Dijkastra.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+#define number 6
+
+int matrix[number][number]; // weight
+int parent[number];
+int visited[number];
+int dist[number]; //與source的距離
+
+void dijkstra(int source) {
+    dist[source] = 0;
+    while (true) {
+        int vertex = -1; //當前節點
+        int near = 1e8;  //找目前最短距離
+        //挑選
+        for (int i = 0; i < number; i++) {
+            if (!visited[i] && dist[i] < near) {
+                vertex = i;
+                near = dist[i];
+            }
+        }
+        if (vertex == -1) { //找不到節點 搜尋完畢
+            break;
+        }
+        visited[vertex] = true;
+        //把 vertex 附近 且未曾走過的edge做 relaxtion
+        for (int i = 0; i < number; i++) {
+            if (!visited[i] && dist[i] > dist[vertex] + matrix[vertex][i]) {
+                dist[i] = dist[vertex] + matrix[vertex][i];
+                parent[i] = vertex;
+            }
+        }
+    }
+}
+void init() {
+    for (int i = 0; i < number; i++) {
+        for (int j = 0; j < number; j++)
+            matrix[i][j] = 1e8;
+    }
+    for (int i = 0; i < number; i++) {
+        parent[i] = i; //自己是parent
+    }
+    for (int i = 0; i < number; i++) {
+        dist[i] = 1e8;
+    }
+}
+int main() {
+    init();
+    matrix[0][1] = matrix[1][0] = 1;
+    matrix[0][2] = matrix[2][0] = 5;
+    matrix[1][2] = matrix[2][1] = 7;
+    matrix[1][3] = matrix[3][1] = 4;
+    matrix[2][4] = matrix[4][2] = 2;
+    matrix[3][5] = matrix[5][3] = 5;
+    int source = 0;
+    dijkstra(source);
+    //看5到0的最短路徑
+    int vertex = 5;
+    cout << "reversed path: ";
+    while (true) {
+        if (vertex == source) {
+            break;
+        } else if (vertex == vertex) {
+            cout << "cannot reach!";
+            break;
+        } else {
+            vertex = parent[vertex];
+            cout << vertex << " ";
+        }
+    }
+    // output: 3 1 0
+    return 0;
+}
